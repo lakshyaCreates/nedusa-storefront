29182734a6cc40f6c513f7b0a8268bc937ea184a
lakshyaCreates <lakshya.creates07@gmail.com>
2025-01-04 20:10:27 +0530

    feat: starting next app setup

---
+++ src/components/providers/index.tsx
@@ -0,0 +1,19 @@
+import { Toaster } from "@/components/ui/sonner";
+
+import { ThemeProvider } from "./theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem
+                disableTransitionOnChange
+            >
+                {children}
+                <Toaster />
+            </ThemeProvider>
+        </>
+    );
+}
